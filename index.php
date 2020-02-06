861d4d0f04bdbc9cc66b2cdcf922dd874d17dd39
patryk-janik <patryk.janik@castingnetworks.com>
2020-02-07 00:51:47 +0100

    Added main page functionalitites

--- index.php
+++ index.php
@@ -18,9 +18,27 @@ get_header();
         <img id="appsome-logo" src="<?php echoImage('AppsomeLogo.svg') ?>" alt="Red Shape Top" />
         <div id="centered-logo-text">
             <h6>Appsome-Solutions Blog</h6>
-            <div class="subtitle1" >We describe product delivery process directly from the kitchen<div>
+            <div class="subtitle1" >We describe product delivery process directly from the kitchen</div>
         </div>
     </div>
+    <div id="posts-list">
+        <?php 
+            $posts = get_posts(array(
+                "posts_per_page" => 10,
+            )); 
+
+            foreach ($posts as $post) {
+                echo "<h6>" . $post->post_title . "</h6>";
+                echo "<div class='caption'>" . $post->post_date . " ☕ " . do_shortcode('[rt_reading_time postfix="min read" postfix_singular="min read"]') ."</div>";
+                echo "<p>" . $post->post_excerpt . "</p>";
+            }
+
+        ?>
+    </div>
+    <footer>
+        <a href="https://www.appsome-solutions.com/">VISIT WEBSITE</a>
+        <a href="https://github.com/appsome-solutions">GITHUB</a>
+    </footer>
 </main>
 
 <?php
