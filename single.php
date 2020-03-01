d60de2f95aa4e7eced78d9855a03c35fcb5efeea
Patryk Janik <pumanitro@gmail.com>
2020-03-01 15:08:35 +0100

    Added subscribe to the newsletter section

--- single.php
+++ single.php
@@ -33,6 +33,34 @@ if ( have_posts() ) {
 
 ?>
 
+<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://appsome-solutions.us19.list-manage.com/subscribe/post?u=8ad4819cfce6f420b3130c0ef&amp;id=6515159370" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+    <div id="subscribe-wrapper">
+        <div>
+            <h6> Subscribe to the newsletter </h6>
+            <div>Subscribe to get ours latest content by email.</div>
+        </div>
+        <div id="subscribe-email-wrapper">
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email" required>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8ad4819cfce6f420b3130c0ef_6515159370" tabindex="-1" value=""></div>
+            <div class="clear"><input type="submit" value="SUBSCRIBE" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </div>
+    </div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
 </main><!-- #site-content -->
 
 
