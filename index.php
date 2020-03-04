4502865d3a32666d8430770a8f8b7b56556e245c
Patryk Janik <pumanitro@gmail.com>
2020-03-04 14:00:34 +0100

    Added RWD for whole blog

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ get_header();
     <a id="logo-wrapper" href="<?php echo get_home_url() ?>">
         <img id="appsome-logo" src="<?php echoImage('AppsomeLogo.svg') ?>" alt="Red Shape Top" />
         <div id="centered-logo-text">
-            <h6>Appsome-Solutions Blog</h6>
+            <h6>Appsome Solutions Blog</h6>
             <div class="subtitle1" >We describe product delivery process directly from the kitchen</div>
         </div>
     </a>
